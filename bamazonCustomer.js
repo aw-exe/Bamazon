9ad97c91e850586854dc6a396c714d1deab4dc94
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-12-15 19:33:36 -0600

    In my javascript file, I added the sql connection code so my database and javascript file are connected.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,25 @@
   
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "root",
+    database: "bamazon"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log(connection);
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });
+  
