c4aff6352bfff7530bb90a6d223ea04ab9cbec9f
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-12-15 23:52:02 -0600

    I coded myself into a hole...I am not a smart lady

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,90 +1,97 @@
-  
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const consoleTable = require("console.table");
+  const mysql = require("mysql");
+  const inquirer = require("inquirer");
+  const consoleTable = require("console.table");
 
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log(connection);
-    console.log("connected as id " + connection.threadId);
-    connection.end();
+  const connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "root",
+      database: "bamazon"
   });
 
-const display = function() {
-    connection.query("SELECT * FROM products", function(err, results) {
-        if (err) throw err;
-        console.table(results);
-    })
-};
+  connection.connect(function (err) {
+      if (err) throw err;
+      console.log(connection);
+      console.log("connected as id " + connection.threadId);
+      display();
+      connection.end();
+  });
 
-let run = function() {
-    // query database for all products available for purchase
-    connection.query("SELECT * FROM products", function(err, results) {
-        if (err) throw err;
-        // prompt user which item they would like to purchase
-        inquirer.prompt([
-            {
-                name: "product",
-                type: "list",
-                choices: function() {
-                    var choiceArray = [];
-                    for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].product_name);
-                    }
-                    return choiceArray;
-                },
-                message: "What product would you like to buy?"
-            },
-            {
-                name: "amount",
-                type: "input",
-                message: "How many would you like?"
-            }
-        ]).then(function(answer) {
-            let chosenProduct;
-            for (let i = 0; i < results.length; i++) {
-                if (answer.product === results[i].product_name) {
-                    chosenProduct = results[i];
-                }
-            }
+  const display = function () {
+      connection.query("SELECT * FROM products", function (err, results) {
+          if (err) throw err;
+          console.table(results);
+          run();
+      })
+  };
 
-            if (chosenProduct.stock_quantity > parseInt(answer.amount)) {
-                connection.query("UPDATE products WHERE?", [
-                {
-                    stock_quantity: chosenProduct.stock_quantity - parseInt(answer.amount)
-                },
-                {
-                    id: chosenProduct.id
-                }], function(error) {
-                    if (error) throw err;
-                    console.log("\n\n");
-                    console.log("Product purchased successfully!");
-                    console.log("Purchase Summary");
-                    console.log("Item Name: " +  chosenProduct.product_name);
-                    console.log("Item Count: " + parseInt(answer.amount));
-                    console.log("Total: " + "$" + (chosenProduct.price * parseInt(answer.amount)));
-                    console.log("\n\n");
-                    display();
-                    run();
-                })
-            } else {
-                console.log("Insuficient quantity! Please pick another item.");
-                display();
-                run();
-            }
-        });
-    });
-};
+  let run = function () {
+      // query database for all products available for purchase
+      connection.query("SELECT * FROM products", function (err, results) {
+          if (err) throw err;
+          // prompt user which item they would like to purchase
+          inquirer.prompt([{
+                  name: "product",
+                  type: "list",
+                  choices: function () {
+                      var choiceArray = [];
+                      for (var i = 0; i < results.length; i++) {
+                          choiceArray.push(results[i].product_name);
+                      }
+                      return choiceArray;
+                  },
+                  message: "What product would you like to buy?"
+              },
+              {
+                  name: "amount",
+                  type: "input",
+                  message: "How many would you like?"
+              }
+          ]).then(function (answer) {
+              let chosenProduct;
+              for (let i = 0; i < results.length; i++) {
+                  if (answer.product === results[i].product_name) {
+                      chosenProduct = results[i];
+                  }
+              }
+              checkOrder(chosenProduct, answer);
+          });
+      });
+  }
 
-display();
-run();
+  let checkOrder = function (chosenProduct, answer) {
 
+      if (chosenProduct.stock_quantity > answer.amount) {
+          console.log(answer)
+          connection.query(`UPDATE products SET stock_quantity = stock_quantity - ${answer.amount} WHERE item_id = ${chosenProduct.item_id}`);
+          // {
+          //     // stock_quantity: chosenProduct - parseInt(answer.amount)
+
+          // },
+          // {
+          //     id: chosenProduct.id
+
+          console.log("\n\n");
+          console.log("Product purchased successfully!");
+          console.log("Purchase Summary");
+          console.log("Item Name: " + chosenProduct.product_name);
+          console.log("Item Count: " + parseInt(answer.amount));
+          console.log("Total: " + "$" + (chosenProduct.price * parseInt(answer.amount)));
+          console.log("\n\n");
+          // display();
+          // run();
+
+      } else {
+          console.log("Insuficient quantity! Please pick another item.");
+          // display();
+          // run();
+      }
+  }
+
+
+
+
+  display();
+  run();
